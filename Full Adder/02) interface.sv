7bc58bfe88dfc154262ddbe1482e7e7c3c9bc698
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-17 22:59:43 +0530

    Create 02) interface.sv

---
+++ Full Adder/02) interface.sv
@@ -0,0 +1,14 @@
+`ifndef fa_interface_sv
+`define fa_interface_sv
+
+interface fa_intf;
+     logic a;
+     logic b;
+     logic c;
+     logic sum;
+     logic carry;
+  
+  event drv_done;
+endinterface
+
+`endif
