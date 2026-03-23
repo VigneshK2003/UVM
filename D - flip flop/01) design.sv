bb9bcfcd12b7b51894fc750f47a50caa7dac7a92
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-03-23 22:35:18 +0530

    Create 01) design.sv

---
+++ D - flip flop/01) design.sv
@@ -0,0 +1,14 @@
+// d flip flop
+
+module d_flip_flop(
+   input clk, rst, d,
+  output reg q );
+  
+  always@(posedge clk) begin
+    if(rst)
+      q <= 1'b0;
+     else
+       q <= d;
+  end
+endmodule
+  
